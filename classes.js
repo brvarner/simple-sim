812027efb3ffe94d523b0506b46ff1935cf1da9e
brvarner <bvarner4592@gmail.com>
2023-12-18 08:02:48 -0600

    added structuredClones to make generating new WRs easier, completed function to create and display three receivers, now adding that functionality for corners and finishing functions

--- classes.js
+++ classes.js
@@ -6,18 +6,31 @@ fetch("./names.json")
   });
 
 export class Receiver {
-  name;
-  skills;
+  firstName = Object.values(nameData.wrFirstNames[(Math.random() * 10) | 0])[0];
+  lastName = Object.values(nameData.wrLastNames[(Math.random() * 10) | 0])[0];
 
-  firstName = Object.values(nameData.wrFirstNames[(Math.random() * 10) | 0]);
-  lastName = Object.values(nameData.wrLastNames[(Math.random() * 10) | 0]);
-
-  constructor({ skills }) {
-    this.name = this.firstName + this.lastName;
+  constructor(skills) {
+    this.name = `${this.firstName} ${this.lastName}`;
     this.wrSpeed = skills.wrSpeed;
     this.wrAccel = skills.wrAccel;
     this.routeR = skills.routeR;
     this.wrCatch = skills.wrCatch;
+  }
+
+  getSpeed() {
+    return this.wrSpeed;
+  }
+
+  getAccel() {
+    return this.wrAccel;
+  }
+
+  getRouteR() {
+    return this.routeR;
+  }
+
+  getCatch() {
+    return this.wrCatch;
   }
 
   runShortRoute() {}
@@ -30,18 +43,31 @@ export class Receiver {
 }
 
 export class Corner {
-  name;
-  skills;
+  firstName = Object.values(nameData.cbFirstNames[(Math.random() * 10) | 0])[0];
+  lastName = Object.values(nameData.cbLastNames[(Math.random() * 10) | 0])[0];
 
-  firstName = Object.values(nameData.cbFirstNames[(Math.random() * 10) | 0]);
-  lastName = Object.values(nameData.cbLastNames[(Math.random() * 10) | 0]);
-
-  constructor({ skills }) {
-    this.name = this.firstName + this.lastName;
+  constructor(skills) {
+    this.name = `${this.firstName} ${this.lastName}`;
     this.cbSpeed = skills.cbSpeed;
     this.cbAccel = skills.cbAccel;
     this.cover = skills.cover;
     this.cbCatch = skills.cbCatch;
+  }
+
+  getSpeed() {
+    return this.cbSpeed;
+  }
+
+  getAccel() {
+    return this.cbAccel;
+  }
+
+  getCoverage() {
+    return this.cover;
+  }
+
+  getCatch() {
+    return this.cbCatch;
   }
 
   coverShortRoute() {}
--- main.js
+++ main.js
@@ -1,4 +1,22 @@
 import { Corner, Receiver } from "./classes.js";
+import {
+  statGen,
+  wrWinnerDec,
+  cbWinnerDec,
+  battle,
+  accelStart,
+  accelBattle,
+  coverRouteBattleCovered,
+  offTheLineBattle,
+  coverRouteBattleOpen,
+  shortRoute,
+  medRoute,
+  deepRoute,
+  wrOpenRoll,
+  wrCoveredRoll,
+  throwToCatch,
+} from "./functions.js";
+
 let nameData;
 fetch("./names.json")
   .then((res) => res.json())
@@ -27,7 +45,15 @@ let cbAccel = 0;
 let cover = 0;
 let cbCatch = 0;
 
-let max = Math.floor(100);
+let accelRes;
+let speedRes;
+let coverRes;
+let catchRes;
+
+let accelMarginOfV;
+let speedMarginOfV;
+let coverMarginOfV;
+let catchMarginOfV;
 
 const qbObj = {
   speed,
@@ -63,15 +89,57 @@ const openHandler = {
 
 const openProxy = new Proxy(wrOpen, openHandler);
 
-let accelMarginOfV;
-let speedMarginOfV;
-let coverMarginOfV;
-let catchMarginOfV;
+const wrGen = (obj) => {
+  obj.wrSpeed = statGen(60, 99);
+  obj.wrAccel = statGen(60, 99);
+  obj.routeR = statGen(60, 99);
+  obj.wrCatch = statGen(60, 99);
+};
 
-const statGen = (min, maxArg) => {
-  min = Math.ceil(min);
-  maxArg = Math.floor(maxArg);
-  return Math.floor(Math.random() * (maxArg - min + 1)) + min;
+const wrObj2 = structuredClone(wrObj);
+const wrObj3 = structuredClone(wrObj);
+
+let wrObjArr = [wrObj, wrObj2, wrObj3];
+
+let wr1;
+let wr2;
+let slotWr;
+
+let wrObjFiller = (objArr) => {
+  objArr.forEach((obj) => {
+    wrGen(obj);
+  });
+
+  if (wrObj3.routeR) {
+    wr1 = new Receiver(wrObj);
+    wr2 = new Receiver(wrObj2);
+    slotWr = new Receiver(wrObj3);
+  }
+
+  let display = document.createElement("div");
+  display.id = "wrDisplay";
+
+  document.body.appendChild(display);
+  document.getElementById("wrDisplay").innerHTML = `
+        <h1>WR1 Info</h1>
+        <p> name: ${wr1.name} </p>
+        <p> speed: ${wr1.wrSpeed} </p>
+        <p> accel: ${wr1.wrAccel} </p>
+        <p> route running: ${wr1.routeR} </p>
+        <p> catching: ${wr1.wrCatch} </p>
+        <h1>WR2 Info</h1>
+        <p> name: ${wr2.name} </p>
+        <p> speed: ${wr2.wrSpeed} </p>
+        <p> accel: ${wr2.wrAccel} </p>
+        <p> route running: ${wr2.routeR} </p>
+        <p> catching: ${wr2.wrCatch} </p>
+        <h1>Slot WR Info</h1>
+        <p> name: ${slotWr.name} </p>
+        <p> speed: ${slotWr.wrSpeed} </p>
+        <p> accel: ${slotWr.wrAccel} </p>
+        <p> route running: ${slotWr.routeR} </p>
+        <p> catching: ${slotWr.wrCatch} </p>
+    `;
 };
 
 const qbMaker = (qbObj) => {
@@ -102,18 +170,6 @@ const wrMaker = (wrObj) => {
   obj.wrAccel = statGen(60, 99);
   obj.routeR = statGen(60, 99);
   obj.wrCatch = statGen(60, 99);
-
-  let display = document.createElement("div");
-  display.id = "wrDisplay";
-
-  document.body.appendChild(display);
-  document.getElementById("wrDisplay").innerHTML = `
-        <h1>WR Info</h1>
-        <p> speed: ${obj.wrSpeed} </p>
-        <p> accel: ${obj.wrAccel} </p>
-        <p> route running: ${obj.routeR} </p>
-        <p> catching: ${obj.wrCatch} </p>
-    `;
 };
 
 const cbMaker = (cbObj) => {
@@ -144,243 +200,20 @@ const cbMaker = (cbObj) => {
     `;
 };
 
-const openDisplayUpdate = () => {
-  document.getElementById("wrOpenDisplay").innerHTML = `
-    <h1>WR Open?</h1>
-    <p> ${openProxy.value} </p>
-`;
-};
-
-const wrWinnerDec = (marginOfV) => {
-  if (marginOfV >= 10) {
-    return { "WR CRIT!": marginOfV };
-  } else {
-    return { "WR Win! Margin of Victory": marginOfV };
-  }
-};
-
-const cbWinnerDec = (marginOfV) => {
-  if (marginOfV >= 10) {
-    return { "CB CRIT!": marginOfV };
-  } else {
-    return { "CB Win! Margin of Victory": marginOfV };
-  }
-};
-
-const battle = (obj1Stat, obj2Stat, marginOfV, adv) => {
-  let max = Math.floor(100);
-  let wrMin = Math.ceil(obj1Stat);
-  let cbMin = Math.ceil(obj2Stat);
-
-  let advHolder = Object.keys(adv)[0];
-  let advAmount = Object.values(adv)[0];
-
-  // Working on making the "adv" argument optional
-  if (advHolder.includes("WR")) {
-    wrMin += advAmount;
-  } else if (advHolder.includes("CB")) {
-    cbMin += advAmount;
-  }
-
-  let wrRes = Math.floor(Math.random() * (max - wrMin + 1)) + wrMin;
-  let cbRes = Math.floor(Math.random() * (max - cbMin + 1)) + cbMin;
-
-  if (wrRes > cbRes) {
-    openProxy.value = true;
-    marginOfV = wrRes - cbRes;
-    openDisplayUpdate();
-    return wrWinnerDec(marginOfV);
-  } else if (cbRes > wrRes) {
-    marginOfV = cbRes - wrRes;
-    openDisplayUpdate();
-    return cbWinnerDec(marginOfV);
-  } else {
-    console.log("Push");
-    return { "No Winner": 0 };
-  }
-};
-
-const accelStart = (obj1Stat, obj2Stat, marginOfV) => {
-  let max = Math.floor(100);
-  let wrMin = Math.ceil(obj1Stat);
-  let cbMin = Math.ceil(obj2Stat);
-
-  let wrRes = Math.floor(Math.random() * (max - wrMin + 1)) + wrMin;
-  let cbRes = Math.floor(Math.random() * (max - cbMin + 1)) + cbMin;
-
-  if (wrRes > cbRes) {
-    openProxy.value = true;
-    marginOfV = wrRes - cbRes;
-    return wrWinnerDec(marginOfV);
-  } else if (cbRes > wrRes) {
-    marginOfV = cbRes - wrRes;
-    return cbWinnerDec(marginOfV);
-  } else {
-    console.log("Push");
-    return { "No Winner": 0 };
-  }
-};
-
-let accelRes;
-let speedRes;
-let coverRes;
-let catchRes;
-
-const accelBattle = () => {
-  accelRes = accelStart(wrObj.wrAccel, cbObj.cbAccel, accelMarginOfV);
-  accelMarginOfV = Object.values(accelRes)[0];
-};
-
-const speedBattle = () => {
-  speedRes = battle(wrObj.wrSpeed, cbObj.cbSpeed, speedMarginOfV, accelRes);
-  speedMarginOfV = Object.values(speedRes)[0];
-};
-
-// Need to separate coverage battles from catch battles
-const coverRouteBattleOpen = () => {
-  let cbHand = accelMarginOfV;
-
-  let wrCatchTarget = Math.floor(Math.random() * (max - wrCatch + 1)) + wrCatch;
-  let cbCatchTarget = Math.floor(Math.random() * (max - cbCatch + 1)) + cbCatch;
-
-  let wrCatchCheck = Math.floor(Math.random() * (max - wrCatch + 1)) + wrCatch;
-  let cbCatchCheck =
-    Math.floor(Math.random() * (max - cbCatch + 1)) + cbCatch - cbHand;
-  console.log({ wrCatchCheck, wrCatchTarget });
-  console.log({ cbCatchCheck, cbCatchTarget });
-
-  if (cbCatchCheck >= wrCatchCheck) {
-    alert("PBU");
-  } else if (wrCatchCheck < wrCatchTarget) {
-    alert("Damn, butterfingers!");
-  } else if (wrCatchCheck >= wrCatchTarget) {
-    alert("Ball Caught!");
-  }
-};
-
-const coverRouteBattleCovered = () => {
-  let wrHand = accelMarginOfV;
-
-  let wrCatchTarget = Math.floor(Math.random() * (max - wrCatch + 1)) + wrCatch;
-  let cbCatchTarget = Math.floor(Math.random() * (max - cbCatch + 1)) + cbCatch;
-
-  let wrCatchCheck =
-    Math.floor(Math.random() * (max - wrCatch + 1)) + wrCatch - wrHand;
-  console.log({ wrCatchCheck });
-  let cbCatchCheck = Math.floor(Math.random() * (max - cbCatch + 1)) + cbCatch;
-
-  console.log({ wrCatchCheck, wrCatchTarget });
-  console.log({ cbCatchCheck, cbCatchTarget });
-
-  if (wrCatchCheck >= cbCatchCheck) {
-    alert("GET MOSSED ON!");
-  } else if (cbCatchCheck < cbCatchTarget) {
-    alert("Nice PBU");
-  } else if (cbCatchCheck >= cbCatchTarget) {
-    alert("PICK!");
-  }
-};
-
-let offTheLineBattle = () => {
-  accelBattle();
-  if (Object.values(accelRes)[0] < 10) {
-    // We'd do a speedBattle then have the wrOpen variable update
-    // after half a second
-    setTimeout(() => {
-      speedBattle();
-    }, 500);
-  }
-};
-
-// Ideally, each play will have 2WR - 5WR sets. In your first season, unless you start balling out, you won't have playcalling ability.
-// We'll separate the
-
-const shortRoute = () => {
-  offTheLineBattle();
-};
-
-const medRoute = () => {
-  // Here we take the best two out of three results to determine if a WR is open or not.
-  // The battles will go like this: one accel battle off the line. one speed battle after that,
-  // then the cover v route running battle. Next battle is speed again, then cover v route running.
-  // Last battle is accel, then speed then cover v route running
-};
-
-const deepRoute = () => {
-  // Here we take the best three out of five results to determine if a WR is open or not.
-  //   These battles go in the same basic order as the medRoute battles, with a repeat of battles two and three.
-};
-
-const wrOpenRoll = () => {
-  let throwStrMin = qbObj.armStr;
-  let throwAccMin = qbObj.armAcc;
-
-  let throwTargetStr =
-    Math.floor(Math.random() * (max - throwStrMin + 1)) + throwStrMin;
-  let throwTargetAcc =
-    Math.floor(Math.random() * (max - throwAccMin + 1)) + throwAccMin;
-
-  let strCheck =
-    Math.floor(Math.random() * (max - throwStrMin + 1)) + throwStrMin;
-  let accCheck =
-    Math.floor(Math.random() * (max - throwAccMin + 1)) + throwAccMin;
-
-  if (strCheck >= throwTargetStr) {
-    console.log("str good!");
-    if (accCheck >= throwTargetAcc) {
-      console.log("acc good!");
-      // If the arm str and acc are good, there's no QB penalty in the catch battle
-      // the losing CB gets a penalty to their catch check. They can still get a PBU
-      coverRouteBattleOpen();
-    } else {
-      console.log("Wild Throw");
-    }
-  } else {
-    console.log("Underthrown");
-  }
-};
-
-const wrCoveredRoll = () => {
-  let throwStrMin = qbObj.armStr;
-  let throwAccMin = qbObj.armAcc;
-
-  let throwTargetStr =
-    Math.floor(Math.random() * (max - throwStrMin + 1)) + throwStrMin;
-  let throwTargetAcc =
-    Math.floor(Math.random() * (max - throwAccMin + 1)) + throwAccMin;
-
-  let strCheck =
-    Math.floor(Math.random() * (max - throwStrMin + 1)) + throwStrMin;
-  let accCheck =
-    Math.floor(Math.random() * (max - throwAccMin + 1)) + throwAccMin;
-
-  if (strCheck >= throwTargetStr) {
-    console.log("str good!");
-    if (accCheck >= throwTargetAcc) {
-      console.log("acc good!");
-      coverRouteBattleCovered();
-    } else {
-      console.log("Wild Throw");
-    }
-  } else {
-    console.log("Underthrown");
-  }
-};
-
-const throwToCatch = () => {
-  if (openProxy.value === true) {
-    wrOpenRoll();
-  } else if (openProxy.value === false) {
-    wrCoveredRoll();
-  }
-};
-
 qbButton.addEventListener("click", function () {
   qbMaker(qbObj);
 });
 
 wrButton.addEventListener("click", function () {
-  wrMaker(wrObj);
+  wrObjFiller(wrObjArr);
+
+  console.log(wr1);
+
+  if (wr1) {
+    let speed = wr1.getSpeed();
+
+    console.log(speed);
+  }
 });
 
 cbButton.addEventListener("click", function () {
